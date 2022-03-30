492f02e2a8f0bc03ce78f80300639e06aa7a9c9f
mohadiallo <102606943+mohadiallo@users.noreply.github.com>
2022-03-30 14:13:19 +0200

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,10 +1,46 @@
-[11:27] Housseinatou DIALLO
 #include <stdio.h>
-#include <stdlib.h>int main() {​​
-printf("hello HOUSSEI");
-return 0;
-}​​
+#include <stdlib.h>
 
-[11:27] Housseinatou DIALLO
+int unused_func ()
+{
+	int i;
+	int overrun[10];
+	char *leak;
+	char *unused = "unused string";
 
+	if (i) {
+		printf ("uninitialized variable\n");
+	}
+
+	for (i=0; i<=10; i++) {
+		overrun[i] = i;
+	}
+
+	leak = malloc(256);
+
+	return(0);
+	printf ("structurally dead code\n");
+}
+
+int main (int argc, char **argv)
+{
+	int unused;
+	char *ptr = NULL;
+
+	if (argc < 0) {
+		printf ("abnormal termination\n");
+		abort ();
+	}
+
+	if (ptr != ptr) {
+		printf ("same on both sides\n");
+	}
+
+	if (ptr != NULL) {
+		printf ("logically dead code\n");
+	}
+
+	printf ("hello world\n");
+	return(0);
+}
 
